2777cce439493e86784dd79793da2468a9ea4471
Abdul Syed <dev.abdul01@gmail.com>
2020-10-01 19:13:47 -0400

    add template for signup form

--- flask_user.py
+++ flask_user.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/signup', methods=['POST'])
 def index():
     return "hello world"
 
