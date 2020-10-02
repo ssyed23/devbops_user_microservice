2db2c3414d05327cef4ff394850801d45398d018
Abdul Syed <dev.abdul01@gmail.com>
2020-10-01 20:40:58 -0400

    was able to extract form data

--- flask_user.py
+++ flask_user.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
+from testing import users
 
+test = users()
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    return render_template("signup.html")
-    name = request.form["Username"]
+
+
+@app.route("/")
+def home():
+    return redirect(url_for("signup"))
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == "POST":
+        user = request.form["Username"]
+        return redirect(url_for("user", usr=user))
+
+    
+    else:
+        return render_template("signup.html")
+
+
+
+@app.route("/<usr>")
+def user(usr):
+    return f"hey {usr}"
+ 
+
    
 
 
---
+++ testing.py
@@ -0,0 +1,6 @@
+class users:
+    def __init__(self):
+        pass
+
+    def print(self, name):
+        print(name)
