f2ff2a905a75e3f9fd8be8df8e489fbbd833ac01
Haoran He <hrgutou@gmail.com>
2020-10-09 22:36:59 -0400

    Deployment ready

--- devbops_user_microservice/user_functions.py
+++ devbops_user_microservice/user_functions.py
@@ -15,33 +15,28 @@ class Users:
         self.columns = ["Username", "current city", "current country", "email", "first name", "last name", "password"]
         self.table = self.DB.Table(self.__Tablename__)
 
-
-
     def put(self, user, currentcity, currentcountry, email, firstname, lastname, password):
         all_items = self.table.scan()
         last_primary_key = len(all_items['Items']) + 1
 
         response = self.table.put_item(
-            Item = {
-                self.Primary_Column_Name:last_primary_key,
+            Item={
+                self.Primary_Column_Name: last_primary_key,
                 self.columns[0]: user,
-                self.columns[1] : currentcity,
-                self.columns[2] : currentcountry,
-                self.columns[3] : email,
-                self.columns[4] : firstname,
-                self.columns[5] : lastname,
-                self.columns[6] : self.hash_pw(password)
-
+                self.columns[1]: currentcity,
+                self.columns[2]: currentcountry,
+                self.columns[3]: email,
+                self.columns[4]: firstname,
+                self.columns[5]: lastname,
+                self.columns[6]: self.hash_pw(password)
 
             }
         )
 
         print(response["ResponseMetadata"]["HTTPStatusCode"])
-           
 
-
-
-    def verifying_email_and_user_are_available(self, user, currentcity, currentcountry, email, firstname, lastname, password):
+    def verifying_email_and_user_are_available(self, user, currentcity, currentcountry, email, firstname, lastname,
+                                               password):
         # this function first verfiys that the username and email are available to use
         # if one or the other is not available we return false
         # if available we call our put function that pushes items to db
@@ -50,8 +45,6 @@ class Users:
             return True
         else:
             return False
-            
-
 
     def check_if_user_exists(self, username):
         response = self.table.scan(
@@ -59,23 +52,21 @@ class Users:
         )
         if response["Items"] == []:
             return username
-    
+
     def check_if_user_exists_email(self, email):
         response = self.table.scan(
             FilterExpression=Attr("email").eq(email)
         )
         if response["Items"] == []:
             return email
-    
-    
+
     def hash_pw(self, password):
         hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
         """
         above hased is a byte stream; below we decode back into a striing and save pw as string
         """
-               
-        return hashed.decode("utf-8")
 
+        return hashed.decode("utf-8")
 
     def de_hash(self, password, hashed):
         if bcrypt.checkpw(password, hashed):
@@ -89,7 +80,7 @@ class Users:
         )
 
         ## check if list is emtpy
-        if(len(response["Items"]) > 0):
+        if (len(response["Items"]) > 0):
             # we have find the user
             # perform verification
             hased = response['Items'][0]["password"].encode("utf-8")
@@ -98,7 +89,7 @@ class Users:
 
             verification = self.de_hash(password.encode("utf-8"), hased)
 
-            if(verification):
+            if (verification):
                 return {
                     "Result": True,
                     "Error": None
@@ -115,6 +106,3 @@ class Users:
                 "Result": False,
                 "Error": "Username not found"
             }
-
-
-
