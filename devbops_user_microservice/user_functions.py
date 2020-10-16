1f7775673d66f4ef0dd09c4d46a227b3d30a0e7a
Abdul Syed <dev.abdul01@gmail.com>
2020-10-16 16:08:05 -0400

    crud functions for backend service are complete

--- devbops_user_microservice/user_functions.py
+++ devbops_user_microservice/user_functions.py
@@ -30,7 +30,7 @@ class Users:
             }
         )
 
-        print(response["ResponseMetadata"]["HTTPStatusCode"])
+        # print(response["ResponseMetadata"]["HTTPStatusCode"])
 
     def verifying_email_and_user_are_available(self, user, currentcity, currentcountry, email, firstname, lastname,
                                                password):
@@ -74,18 +74,7 @@ class Users:
             return False
 
 
-    def verify_that_user_exists(self, user, password):
-        response = self.table.scan(
-            FilterExpression=Attr("username").eq(user)
-        )
-        print (response)
-
-
-
-
-
-
-
+   
 
     def authincate_user(self, user, password):
         response = self.table.scan(
@@ -103,7 +92,7 @@ class Users:
             verification = self.de_hash(password.encode("utf-8"), hased)
 
             if (verification):
-                print("it matches")
+                # print("it matches")
                 return {
                     "Result": True,
                     "Error": None,
@@ -111,7 +100,7 @@ class Users:
                     "Country": response['Items'][0]["current country"]
                 }
             else:
-                print("password inncorrect")
+                # print("password inncorrect")
                 return {
                     "Result": False,
                     "Error": "Password incorrect",
@@ -121,7 +110,7 @@ class Users:
 
         else:
             # that means cant find anythign
-            print("no such user")
+            # print("no such user")
             return {
                 "Result": False,
                 "Error": "Username not found",
@@ -149,15 +138,47 @@ class Users:
 
             }
         else:
-            print("user cannot be deleted")
+            # print("user cannot be deleted")
             return{
                  "Result": False,
                  "Error": "user doesnt not exists in data base"
                 }
 
 
-    def update_user_info(self, user):
-        pass
+    def update_user_info(self, user, currentcity, currentcountry, firstname,  lastname):
+        response = self.table.scan(
+            FilterExpression=Attr("username").eq(user)
+        )
+        
+        if len(response["Items"]) > 0:
+            email = response["Items"][0]['email']
+            password = response["Items"][0]['password']
+            # print(password)
+            response = self.table.put_item(
+                Item={
+                    self.Primary_Column_Name: user,
+                    self.columns[0]: currentcity,
+                    self.columns[1]: currentcountry,
+                    self.columns[2]: email,
+                    self.columns[3]: firstname,
+                    self.columns[4]: lastname,
+                    self.columns[5]: password
+
+
+                    }
+                 )
+            return{
+                "Result": True,
+                "Error": None,
+                "Description": "USER info was updated"
+            }
+        else:
+            return{
+                "Result": False,
+                "Error": "USER info was ot updated"
+            }
+
+
 
     def update_user_pw(self, user, password):
         response = self.table.scan(
@@ -166,7 +187,7 @@ class Users:
 
         if len(response["Items"]) > 0:
             # if the response contains a user we bgan to presver dat such as the user city, country, name, etc,
-            print(response["Items"][0])
+            
             email = response["Items"][0]['email']
             currentcity = response["Items"][0]["current city"]
             currentcountry = response["Items"][0]["current country"]
@@ -175,7 +196,6 @@ class Users:
           
             response = self.table.put_item(
                 Item={
-                    self.Primary_Column_Name: user,
                     self.Primary_Column_Name: user,
                     self.columns[0]: currentcity,
                     self.columns[1]: currentcountry,
@@ -193,25 +213,8 @@ class Users:
                 "Description": "USER password updated"
             }
         else:
-            print("nope")
+            # print("nope")
             return{
                 "Result": False,
                 "Error": "USER password was not updated. No such user"
             }
-
-
-    
-
-
-
-
-test = Users()
-# test.verifying_email_and_user_are_available(user="abduls", currentcity="NY", currentcountry="USA", email="desv", firstname="abdul", lastname="syed",
-                                            #    password="password")
-# test.authincate_user(user="abdul", password="password")
-# test.authincate_user(user="abduls", password="123")
-# test.authincate_user(user="abdulsx", password="password123")
-# test.delete_user("abdul")
-
-
-test.update_user_pw(user='abdulssss', password="123")
