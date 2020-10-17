2c3cf7af8df0933bcb89643f695946e4ec94d0c5
dev.rahman96 <dev.rahman96@gmail.com>
2020-10-16 20:56:44 -0400

    Added Jenkins File and Test.py

---
+++ Jenkinsfile
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    stages {
+        stage('build') {
+            steps {
+                   withEnv(["HOME=${env.WORKSPACE}"]) {
+                        sh "pip install flask"
+                        
+                    }
+                }
+        }
+        stage('test') {
+            steps {
+                withEnv(["HOME=${env.WORKSPACE}"]) {
+                    sh 'python3 Test.py'
+                }
+            }
+    
+        }
+    }
+}
---
+++ Test.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+import user_functions 
+
+
+
+class BasicTestCase(unittest.TestCase):
+
+        
+    def api_test(self):
+        pass
+
+    def test_login_route(self):
+        with app.test_client() as c:
+            response = c.get('/login')
+            self.assertEquals(response.status_code, 200)
+            
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 302)
+    
+    
+if __name__ == '__main__':
+    unittest.main()
