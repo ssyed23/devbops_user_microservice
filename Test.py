cda27e626fec4a476fd0ca93ab228c949bc17b81
dev.rahman96 <dev.rahman96@gmail.com>
2020-10-22 20:51:16 -0400

    Added 2 tests

--- Test.py
+++ Test.py
@@ -1,25 +1,117 @@
-import os
 import unittest
+import json
+from datetime import date, datetime
 from devbops_user_microservice import app
+
 
 class BasicTestCase(unittest.TestCase):
 
-        
-    def api_test(self):
-        pass
+    def setUp(self):
+        unittest.TestLoader.sortTestMethodsUsing = None
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+        self.assertEqual(app.debug, False)
+
+    def test_1_viewing(self):
+        rv = self.app.get('/view')
+        data = json.loads(rv.data)
+        assert data['Result'] == True
+
+    def test_2_login(self):
+        req = {
+            "Username": "Mo",
+            "Password": "Test",
+        }
+
+        rv = self.app.post('/login', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == True
+
+    # Result should be false
+    def test_3_Delete(self):
+        req = {
+            "Username": "Mo"
+        }
+
+        rv = self.app.post('/createBlog', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == False
+
+    def test_4_update(self):
+        req = {
+            "BlogName": "QATesting",
+            "New_BlogDate": date.today().strftime("%B %d, %Y"),
+            "New_BlogTime": datetime.now(),
+            "New_BlogContent": "blogBody",
+            "New_BlogLocation": "location"
+        }
+        rv = self.app.post('/update', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == True
+
+    def test_5_NONEXIST_udpate(self):
+        req = {
+            "BlogName": "_QATesting",
+            "New_BlogDate": date.today().strftime("%B %d, %Y"),
+            "New_BlogTime": datetime.now(),
+            "New_BlogContent": "blogBody",
+            "New_BlogLocation": "location"
+        }
+        rv = self.app.post('/update', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == False
+
+    def test_6_commenting(self):
+        req = {
+            "BlogName": "QATesting",
+            "UserName": "QA",
+            "Comment": "comment"
+        }
+        rv = self.app.post('/comment', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == True
+
+    def test_7_NONEXIST_commenting(self):
+        req = {
+            "BlogName": "_QATesting",
+            "UserName": "QA",
+            "Comment": "comment"
+        }
+        rv = self.app.post('/comment', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == False
+
+    def test_8_history(self):
+        req = {
+            "UserName": "QATest"
+        }
+        rv = self.app.post('/history', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == True
+
+
+    def test_9_deleting(self):
+        req = {
+            "BlogName": "QATesting"
+        }
+
+        rv = self.app.post('/delete', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == True
+
+    # Result should be false
+    def test_10_NONEXIST_deleting(self):
+        req = {
+            "BlogName": "QATesting"
+        }
+
+        rv = self.app.post('/delete', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == False
 
 
 
-    def test_login_route(self):
-        with app.test_client() as c:
-            response = c.get('/login')
-            self.assertEquals(response.status_code, 200)
-            
-    def test_index(self):
-        tester = app.test_client(self)
-        response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 302)
-    
-    
 if __name__ == '__main__':
     unittest.main()
