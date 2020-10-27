2946d20fa279ff8c44ed408bbbd070cf0fa71a1c
qsyed <60551618+qsyed@users.noreply.github.com>
2020-10-27 19:28:52 -0400

    Update user_functions.py

--- devbops_user_microservice/user_functions.py
+++ devbops_user_microservice/user_functions.py
@@ -5,16 +5,13 @@ import os
 
 
 
-# dynamodb = boto3.client('dynamodb',region_name='us-east-1',aws_access_key_id=os.environ['AWS_ACCESS_KEY'],aws_secret_access_key=os.environ['AWS_SECRET_KEY'])
-# kms = boto3.client('kms', region_name='us-east-1')
+
 
 class Users:
     def __init__(self):
         # connecting to the dynamdo db 
 
-        # self.__Tablename__ = "user_devbops"
-        # self.client = boto3.client('dynamodb')
-        # self.DB = boto3.resource('dynamodb')
+  
 
         self.__Tablename__ = "DevBops_user"
         self.client = boto3.client('dynamodb')
